815e3d3e1f8137bc6262202f31d46617c30e4551
Shane <bugs5382@users.noreply.github.com>
2023-11-18 22:57:10 -0500

    feat: server listens
    server listener starts up on the port // it does get data, but nothing yet. that's next. we will need the client parser now // unit testing #4 [ci skip]

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Server } from './server.js'
 
 export default Server
 export { Server }
-export { ListenerHandler, Listener, Req, Res } from './listener.js'
+export { Listener, ListenerHandler, ListenerRequest, ListenerResponse } from './listener.js'
 export { HL7ListenerError, HL7ServerError, HL7Error } from './exception.js'
 
 export { ServerOptions, ListenerOptions } from './normalize.js'
