a497374df65a8094e6c70f877574bd5471c38203
Shane <bugs5382@users.noreply.github.com>
2023-12-09 21:51:46 -0500

    feat: lots of updates
    - to many to list, but code from node-hl7-client is being used
    
    #4
    
    [ci skip]

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { Server } from './server.js'
 export default Server
 export { Server }
 export { Listener, ListenerHandler, ListenerRequest, ListenerResponse } from './listener.js'
-export { HL7ListenerError, HL7ServerError, HL7Error } from './exception.js'
+export { HL7ListenerError, HL7ServerError } from './exception.js'
 
 export { ServerOptions, ListenerOptions } from './normalize.js'
