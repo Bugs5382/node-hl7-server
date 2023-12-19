0c388a038565b551d89fdd4eee72a9c6a3746010
Shane <bugs5382@users.noreply.github.com>
2023-12-19 14:04:44 -0500

    feat: revamp the server system
    * TCP/MLLP now being followed by sending and receiving

--- README.md
+++ README.md
@@ -13,8 +13,26 @@ Benefits:
 ## Table of Contents
 
 1. [Acknowledgements](#acknowledgements)
-2. [Basic Install](#install)
-2. [License](#license)
+2. [Keyword Definitions](#Keyword-Definitions)
+3. [Basic Install](#install)
+4. [License](#license)
+
+## Keyword Definitions
+
+This NPM is designed to support medical applications with potential impact on patient care and diagnoses, this package documentation, and it's peer package [node-hl7-server]() follow these definitions when it comes to the documentation.
+
+Keywords such as "MUST", "MUST NOT", "REQUIRED",
+"SHALL", "SHALL NOT", "SHOULD", "SHOULD NOT", "RECOMMENDED", "MAY", and "OPTIONAL".
+These are standardized terms for  technology documentation interoperability.
+These words should have these meaning when you are reading them.
+They might be sans uppercase throughout the documentation, but they would have the same meaning regardless.
+
+* **MUST** - This word, or the terms "**REQUIRED**" or "**SHALL**", mean that the definition is an absolute requirement of the specification.
+* **MUST NOT** - This phrase, or the phrase "**SHALL NOT**", mean that the definition is an absolute prohibition of the specification.
+* **SHOULD** - This word, or the adjective "**RECOMMENDED**", mean that there may exist valid reasons in particular circumstances to ignore a particular item, but the full implications must be understood and carefully weighed before choosing a different course.
+* **SHOULD NOT** - This phrase, or the phrase "**NOT RECOMMENDED**", mean that there may exist valid reasons in particular circumstances when the particular behavior is acceptable or even useful. The full implications should be understood and the case carefully weighed before implementing any behavior described with this label.
+* **MAY** - This word, or the adjective "**OPTIONAL**",  mean that an item is truly optional.  Any implementation which does not include a particular option MUST be prepared to interoperate with another implementation which does include the option, though perhaps with reduced functionality. In the same vein, an implementation which does include a particular option MUST be prepared to interoperate with another implementation, which does not include the option (except, of course, for the feature the option provides.)
+
 
 ## Install
 
--- __tests__/hl7.server.test.ts
+++ __tests__/hl7.server.test.ts
@@ -2,9 +2,8 @@ import fs from "fs";
 import path from "node:path";
 import portfinder from 'portfinder'
 import tcpPortUsed from 'tcp-port-used'
-import {Client, Message} from "node-hl7-client";
-import { Server } from '../src'
-import {expectEvent, sleep} from "./__utils__";
+import {InboundRequest, Server} from '../src'
+import {expectEvent} from "./__utils__";
 
 describe('node hl7 server', () => {
 
@@ -125,6 +124,17 @@ describe('node hl7 server', () => {
       }
     })
 
+    test('error - getMessage() - no message passed to inbound request', async() => {
+      try {
+        // @ts-expect-error no message.
+        const inboundReq = new InboundRequest()
+        // this should cause an error
+        inboundReq.getMessage()
+      } catch (err: any) {
+        expect(err.message).toBe("Message is not defined.")
+      }
+    })
+
   })
 
   describe('basic server tests', () => {
@@ -190,102 +200,6 @@ describe('node hl7 server', () => {
       expect(usedCheck).toBe(true)
 
       await listener.close()
-    })
-
-  })
-
-  describe('end to end tests', () => {
-
-    describe('...send message, get proper ACK', () => {
-
-      let LISTEN_PORT: number
-      beforeEach(async () => {
-        LISTEN_PORT = await portfinder.getPortPromise({
-          port: 3000,
-          stopPort: 65353
-        })
-      })
-
-      test('...no tls', async () => {
-
-        const server = new Server({bindAddress: '0.0.0.0'})
-        const IB_ADT = server.createInbound({port: LISTEN_PORT}, async (req, res) => {
-          const messageReq = req.getMessage()
-          expect(messageReq.get('MSH.12').toString()).toBe('2.7')
-          await res.sendResponse("AA")
-        })
-
-        await sleep(5)
-
-        const client = new Client({host: '0.0.0.0'})
-
-        const OB_ADT = client.createOutbound({ port: LISTEN_PORT }, async (res) => {
-          expect(res.toString()).not.toContain('ADT^A01^ADT_A01')
-        })
-
-        await sleep(5)
-
-        let message = new Message({
-          messageHeader: {
-            msh_9_1: "ADT",
-            msh_9_2: "A01",
-            msh_10: 'CONTROL_ID'
-          }
-        })
-
-        await OB_ADT.sendMessage(message)
-
-        await sleep(10)
-
-        await OB_ADT.close()
-        await IB_ADT.close()
-
-      })
-
-      test('...tls', async () => {
-
-        const server = new Server(
-          {
-            bindAddress: '0.0.0.0',
-            tls:
-              {
-                key: fs.readFileSync(path.join('certs/', 'server-key.pem')),
-                cert: fs.readFileSync(path.join('certs/', 'server-crt.pem')),
-                rejectUnauthorized: false
-              }
-          })
-        const IB_ADT = server.createInbound({port: LISTEN_PORT}, async (req, res) => {
-          const messageReq = req.getMessage()
-          expect(messageReq.get('MSH.12').toString()).toBe('2.7')
-          await res.sendResponse("AA")
-        })
-
-        await sleep(5)
-
-        const client = new Client({host: '0.0.0.0', tls: { rejectUnauthorized: false }})
-        const OB_ADT = client.createOutbound({ port: LISTEN_PORT }, async (res) => {
-          expect(res.toString()).not.toContain('ADT^A01^ADT_A01')
-        })
-
-        await sleep(5)
-
-        let message = new Message({
-          messageHeader: {
-            msh_9_1: "ADT",
-            msh_9_2: "A01",
-            msh_10: 'CONTROL_ID'
-          }
-        })
-
-        await OB_ADT.sendMessage(message)
-
-        await sleep(10)
-
-        await OB_ADT.close()
-        await IB_ADT.close()
-
-      })
-
     })
 
   })
