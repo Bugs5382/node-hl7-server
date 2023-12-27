d125a95fa1d393fb6b346b12481cb8f6fe5104f2
Shane <bugs5382@users.noreply.github.com>
2023-12-27 13:22:58 -0500

    fix: default export back in the code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export { Server } from './server/server.js'
+import { Server } from './server/server.js'
+
+export default Server
+export { Server }
 export { Hl7Inbound, InboundHandler } from './server/hl7Inbound.js'
 export { HL7ListenerError, HL7ServerError } from './utils/exception.js'
 export { ServerOptions, ListenerOptions } from './utils/normalize.js'
