f745cebe8e93c2ebb4a14211abf9cb6be4dcdc5e
Seán Bryceland <sean.bryceland@echoiq.ai>
2024-06-25 11:13:11 +1000

    fix(60): Added test to replicate the issue

--- __tests__/hl7.end2end.test.ts
+++ __tests__/hl7.end2end.test.ts
@@ -3,7 +3,7 @@ import path from "node:path";
 import { describe, expect, test } from 'vitest';
 import tcpPortUsed from 'tcp-port-used'
 import Client, {Batch, Message} from "node-hl7-client";
-import {createDeferred, expectEvent} from './__utils__'
+import {createDeferred, expectEvent, generateLargeBase64String} from './__utils__'
 import Server from "../src";
 
 describe('node hl7 end to end - client', () => {
@@ -288,4 +288,69 @@ describe('node hl7 end to end - client', () => {
 
   })
 
-})
+  describe("server/client large data checks", () => {
+    test("...large encapsulated data", async () => {
+      let dfd = createDeferred<void>();
+
+      const server = new Server({ bindAddress: "0.0.0.0" });
+      const listener = server.createInbound(
+        { port: 3000 },
+        async (req, res) => {
+          const messageReq = req.getMessage();
+          expect(messageReq.get("MSH.12").toString()).toBe("2.7");
+          await res.sendResponse("AA");
+          const messageRes = res.getAckMessage();
+          expect(messageRes?.get("MSA.1").toString()).toBe("AA");
+        }
+      );
+
+      await expectEvent(listener, "listen");
+
+      const client = new Client({ host: "0.0.0.0" });
+
+      const outbound = client.createConnection({ port: 3000 }, async (res) => {
+        const messageRes = res.getMessage();
+        expect(messageRes.get("MSA.1").toString()).toBe("AA");
+        dfd.resolve();
+      });
+
+      await expectEvent(outbound, "connect");
+
+      let message = new Message({
+        messageHeader: {
+          msh_9_1: "ADT",
+          msh_9_2: "A01",
+          msh_10: "CONTROL_ID",
+          msh_11_1: "D",
+        },
+      });
+
+      // Add Patient level observation result
+      const OBX = message.addSegment("OBX"); // Adds the OBX as a segment for this MSH.
+      OBX.set("1", "1"); // Sequence ID
+      OBX.set("2", "ED"); // Type Encapsulated Data
+      OBX.set("3.1", "SOME-PDF"); // Identifier (max 20 chars)
+      OBX.set("3.2", "Result Report"); // Title (max 199 chars)
+      OBX.set("3.3", "99COD"); // Some Coding System
+      OBX.set("5.2", "application"); // Type of referenced data
+      OBX.set("5.3", "pdf"); // Data Sub type
+      OBX.set("5.4", "Base64"); // PDF Data
+      OBX.set("5.5", generateLargeBase64String(300)); // PDF Data
+      OBX.set("8", "A"); // (A)bnormal or (N)ormal result
+      OBX.set("11", "F"); // Final result or correction to final result
+      OBX.set("14", "20240625103600"); // Result creation time
+
+      await outbound.sendMessage(message);
+
+      await dfd.promise;
+
+      expect(client.totalSent()).toEqual(1);
+      expect(client.totalAck()).toEqual(1);
+
+      await outbound.close();
+      await listener.close();
+
+      client.closeAll();
+    });
+  });
+});
